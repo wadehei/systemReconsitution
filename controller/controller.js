93bc8b6d5ebbbe32041623dc92d16e0c3b4afb2f
wadehei <wadehei@163.com>
2020-02-20 21:07:26 +0800

    控制台遮罩添加

--- controller/controller.js
+++ controller/controller.js
@@ -4,11 +4,13 @@
             $('.controller_content').removeClass('controller_open').addClass('controller_close');
             setTimeout(() => {
                 $('.controller_content').empty();
+            $('.controller_bg').removeClass('controller_bg_shade');
             }, 500);
         } else {
             var controllerHtml = template("menuIdOne", { controllerList: controllerList.data })
             $('.controller_content').html(controllerHtml);
             $('.controller_content').removeClass('controller_close').addClass('controller_open');
+            $('.controller_bg').addClass('controller_bg_shade');
         }
     }
     $.fn.hj_controller = function () {
